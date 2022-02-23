e6df82b6234cdbb5eebe2c375ca891c5cd169628
Santiago L Martinez <slm17slm17@gmail.com>
2022-02-23 04:27:26 -0300

    robot y usuario pueden elegir p p t

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ const _piedraBtn = document.querySelector('.piedra')
 const _papelBtn = document.querySelector('.papel')
 const _tijerasBtn = document.querySelector('.tijeras')
 const _robotChoise = document.querySelector('.piedra')
+// let robotCh = Math.floor(Math.random() *3) + 1 
 
 const piedraValue = 1
 const papelValue = 2
@@ -11,38 +12,63 @@ const tijerasValue = 3
 
 _piedraBtn.addEventListener('click', (e)=>{
     // console.log ('diste click en piedra')
-    playerChoise(piedraValue)
+    
+    winner(playerChoise(piedraValue), choiseRobot() )
 })
 _papelBtn.addEventListener('click', (e)=>{
     // console.log ('diste click en papel')
-    playerChoise(papelValue)
+    winner(playerChoise(papelValue), choiseRobot() )
 
 })
 
 _tijerasBtn.addEventListener('click', (e)=>{
     // console.log ('diste click en tijeras')
-    playerChoise(tijerasValue)
+    winner(playerChoise(tijerasValue), choiseRobot() )
 
 })
 
 // function
 const playerChoise = (choisePlayer) =>{
-    winner(choisePlayer, choiseRobot() )
-    return console.log('Player:',  choisePlayer)
+    // winner(choisePlayer, choiseRobot() )
+    console.log('Player :', choisePlayer)
+    return  choisePlayer
 }
 
-const choiseRobot = () =>{
+const choiseRobot = (robotCh) =>{
      robotCh = Math.floor(Math.random() *3) + 1 
-    //  console.log('Robot :',robotCh)
-    return console.log('Robot :', robotCh);
+    console.log('Robot :',robotCh)
+    return  robotCh;
 }
 
 const winner = (player, robot) =>  {
-    if(player === papelValue && robot === piedraValue){
-        console.log('papel pierde contra tijeras')
+
+    if(player === 2 && robot === 1){
+        console.log('papel gana contra piedras')
         return 
     }
+    if(player === 2 && robot === 3){
+        console.log('papel pierde contra tijeras')
+        return  console.log('papel pierde contra tijeras')
+    }
+    if(player === 1 && robot === 3){
+        console.log('piedra gana contra tijeras')
+        return 
+    }
+    if(player === 1 && robot === 3){
+        console.log('piedra pierde contra papel')
+        return 
+    }
+    if(player === 3 && robot === 1){
+        console.log('tijeras pierde contra piedra')
+        return 
+    }
+    if(player === 3 && robot === 2){
+        console.log('tijeras gana contra papel')
+        return 
+    }
+    if(player === robot){
+        console.log('Empate')
+    }
 
-    // console.log( choiseRobot())
 
 }
