1fdad1efe70b619f47f553ea6f0be94650ef879e
Santiago L Martinez <slm17slm17@gmail.com>
2022-02-23 05:23:36 -0300

    se mejora sintaxis del codigo

--- js/main.js
+++ js/main.js
@@ -1,12 +1,21 @@
+// tablero
 const _piedraBtn = document.querySelector('.piedra')
 const _papelBtn = document.querySelector('.papel')
 const _tijerasBtn = document.querySelector('.tijeras')
+// robot
+const _robotChoiseContainer = document.querySelector('.robotChoiseContainer')
 const _robotChoise = document.querySelector('.piedra')
-_robotChoiseContainer = document.querySelector('.robotChoiseContainer')
-
+const _robotImg = document.querySelector('.robotImg')
+// puntos
+const _userPoints = document.querySelector('.userPoints')
+const _robotPoints = document.querySelector('.robotPoints')
+// values
 const piedraValue = 1
 const papelValue = 2
 const tijerasValue = 3
+
+let userPoints = 0
+let robotPoints = 0
 
 // events listener
 
@@ -20,19 +29,20 @@ _papelBtn.addEventListener('click', (e)=>{
     winner(playerChoise(papelValue), choiseRobot() )
 
 })
-
 _tijerasBtn.addEventListener('click', (e)=>{
     // console.log ('diste click en tijeras')
     winner(playerChoise(tijerasValue), choiseRobot() )
 
 })
 
-// function
+// functions
+
+// player
 const playerChoise = (ch) =>{
     console.log('Player :', ch)
     return  ch
 }
-
+// robot
 const choiseRobot = (ch) =>{
      ch = Math.floor(Math.random() *3) + 1 
     console.log('Robot :',ch)
@@ -40,31 +50,43 @@ const choiseRobot = (ch) =>{
 
     return  ch;
 }
-
+// elegir ganador
 const winner = (player, robot) =>  {
 
     if(player === 2 && robot === 1){
         console.log('papel gana contra piedras')
+        _userPoints.textContent = `${ userPoints = userPoints + 1 }`
         return 
     }
+
     if(player === 2 && robot === 3){
         console.log('papel pierde contra tijeras')
-        return  console.log('papel pierde contra tijeras')
+        _robotPoints.textContent = `${robotPoints = robotPoints + 1 }`
+        return  
     }
+
     if(player === 1 && robot === 3){
         console.log('piedra gana contra tijeras')
+        _userPoints.textContent = `${userPoints = userPoints +1}`
+
         return 
     }
-    if(player === 1 && robot === 3){
+    if(player === 1 && robot === 2){
         console.log('piedra pierde contra papel')
+        _robotPoints.textContent = `${robotPoints = robotPoints +1 }`
+
         return 
     }
     if(player === 3 && robot === 1){
         console.log('tijeras pierde contra piedra')
+        _robotPoints.textContent = `${robotPoints = robotPoints  +1}`
+
         return 
     }
     if(player === 3 && robot === 2){
         console.log('tijeras gana contra papel')
+        _userPoints.textContent = `${userPoints = userPoints +1 }`
+
         return 
     }
     if(player === robot){
