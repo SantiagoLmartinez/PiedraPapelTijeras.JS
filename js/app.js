f2ffc488bc05c9e428b9a28c800711508e1a407e
Santiago L Martinez <slm17slm17@gmail.com>
2022-02-25 02:12:19 -0300

    proyecto por terminar

---
+++ js/app.js
@@ -0,0 +1,156 @@
+const piedra = 1
+const papel = 2
+const tijeras = 3
+
+let userPoints = 0
+let robotPoints = 0
+
+// tablero
+const _piedraBtn = document.querySelector('.piedra')
+const _papelBtn = document.querySelector('.papel')
+const _tijerasBtn = document.querySelector('.tijeras')
+
+const _machine = document.getElementById('machine')
+
+const _userJugada = document.getElementById('userjugada')
+const _robotJugada = document.getElementById('robotJugada')
+
+
+const playerChoise = (ch) =>{
+    console.log('User: ',ch)
+    return  ch
+
+}
+
+// robot play 
+const robotChoise = () =>{
+    const _spinner = document.getElementById('spinner')
+    // const _machine = document.getElementById('machine')
+    ch = Math.floor(Math.random() *3) + 1 
+
+    _spinner.classList.remove('d-none')   
+    _machine.innerHTML = ''     
+        setTimeout( 
+        function(){
+            
+            _spinner.classList.add('d-none')
+            
+            if(ch === 1){
+                _machine.innerHTML = `
+                <img id="piedra" class="" src="svg/piedra.svg" alt="piedra">`
+                _robotJugada.innerHTML = 'Piedra'
+            }
+
+            if(ch === 2){
+                _machine.innerHTML = '<img id="piedra" class="" src="svg/papel.svg" alt="piedra">'
+                _robotJugada.innerHTML = 'Papel'
+
+                // console.log('papel')
+            }
+
+            if(ch === 3){
+                _machine.innerHTML = '<img id="piedra" class="" src="svg/tijeras.svg" alt="piedra">'
+                _robotJugada.innerHTML = 'Tijeras'
+                
+                // console.log('tijeras')
+            }
+        },1000)
+
+
+
+    console.log('Robot: ',ch)
+    return ch 
+}
+
+// select winner
+
+const winner = (playerChoise, robotChoise) =>{
+    // points()
+    setTimeout( 
+        function(){
+            if(playerChoise === robotChoise){
+                // _machine.innerHTML = `<h3>EMPATE</h3>`
+                
+                return console.log('Empate')  
+            }
+            
+            else if(playerChoise === piedra ){
+                if(robotChoise === 2){
+                    robotPoints++
+                    points()
+        
+                    return  console.log('user:Piedra PIERDE contra robot:Papel')}
+                if(robotChoise === 3){
+                    userPoints++
+                    points()
+        
+                    return console.log('user:Piedra GANA contra robot: Tijeras')}
+        
+            }else if(playerChoise === papel ){
+                if(robotChoise === 3){
+                    robotPoints++
+                    points()
+        
+                    return console.log('user: Papel PIERDE contra robot:Tijeras')}
+                if(robotChoise === 1){
+                    userPoints++ 
+                    points()
+        
+                    return console.log('user: Papel GANA contra robot: Piedra')}
+        
+            }else if(playerChoise === tijeras ){ 
+                if(robotChoise === 1){
+                    robotPoints++
+                    points()
+        
+                    return console.log('user: Tijeras PIERDE contra robot: Piedra')}
+                if(robotChoise === 2){
+                    userPoints++ 
+                    points()
+        
+                    return console.log('user: Tijeras GANA contra robot: Papel')}
+            }
+        },1000)
+}
+
+const points = () =>{
+    // puntos
+    const _userPoints = document.querySelector('.userPoints')
+    const _robotPoints = document.querySelector('.robotPoints')
+
+    _userPoints.innerHTML = userPoints
+    _robotPoints.innerHTML = robotPoints
+
+    if(userPoints === 3){
+        alert('GANASTE')
+    }
+    
+    if(robotPoints === 3){
+        alert('Perdiste')
+    }
+}
+
+
+//Player play 
+_piedraBtn.addEventListener('click', (e)=>{
+    _userJugada.innerHTML = 'Piedra'
+    // console.log ('diste click en piedra')
+    winner(playerChoise(piedra), robotChoise())
+
+})
+
+_papelBtn.addEventListener('click', (e)=>{
+    _userJugada.innerHTML = 'Papel'
+
+    // console.log ('diste click en papel')
+    winner(playerChoise(papel), robotChoise() )
+})
+
+_tijerasBtn.addEventListener('click', (e)=>{
+    _userJugada.innerHTML = 'Tijeras'
+
+    // console.log ('diste click en tijeras')
+    winner(playerChoise(tijeras), robotChoise() )
+})
+
+// points()
--- js/main.js
+++ js/main.js
@@ -10,30 +10,30 @@ const _robotImg = document.querySelector('.robotImg')
 const _userPoints = document.querySelector('.userPoints')
 const _robotPoints = document.querySelector('.robotPoints')
 // values
-const piedraValue = 1
-const papelValue = 2
-const tijerasValue = 3
+const piedra = 1
+const papel = 2
+const tijeras = 3
 
 let userPoints = 0
-
-
 let robotPoints = 0
 
+// spinner
+const _spinner = document.querySelector('#spinner')
 // events listener
 
 _piedraBtn.addEventListener('click', (e)=>{
     // console.log ('diste click en piedra')
     
-    winner(playerChoise(piedraValue), choiseRobot() )
+    winner(playerChoise(piedra), choiseRobot() )
 })
 _papelBtn.addEventListener('click', (e)=>{
     // console.log ('diste click en papel')
-    winner(playerChoise(papelValue), choiseRobot() )
+    winner(playerChoise(papel), choiseRobot() )
 
 })
 _tijerasBtn.addEventListener('click', (e)=>{
     // console.log ('diste click en tijeras')
-    winner(playerChoise(tijerasValue), choiseRobot() )
+    winner(playerChoise(tijeras), choiseRobot() )
 
 })
 
@@ -106,19 +106,29 @@ const winner = (player, robot) =>  {
 
 // pintar en pantalla que eligio el robot
 const printRobotChoise = (ch) =>{
-    _robotChoiseContainer.innerHTML= ''
-    if(ch === 1){
-        _robotChoiseContainer.innerHTML= `<img class="" src="svg/stone-576268.svg" alt="tijeras">`
+    _robotChoiseContainer.innerHTML = ''
+
+    // setTimeout( 
+    //     function(){
+    //         _robotChoiseContainer.innerHTML = '<div class="spinner-border d-none" role="status">'
+    //     _spinner.classList.as('d-none')   
+    //     },2000)
+
+        // _spinner.classList.add('d-none')
+
+    // _robotImg.src
+        if(ch === 1){
+        _robotChoiseContainer.innerHTML= `<img class="" src="svg/piedra.svg" alt="piedra">`
         return
     }
     if(ch === 2){
         _robotChoiseContainer.innerHTML= `
-        <img class="" src="svg/scroll-34696.svg" alt="tijeras">
+        <img class="" src="svg/papel.svg" alt="papel">
         `
         return
     }
     if(ch === 3){
-        _robotChoiseContainer.innerHTML= `<img class="" src="svg/scissors-1297454.svg" alt="tijeras">`
+        _robotChoiseContainer.innerHTML= `<img class="" src="svg/tijeras.svg" alt="tijeras">`
         return
     }
     // _robotChoiseContainer.innerHTML= 
